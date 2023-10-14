55221c4b581720dcbc30893194befeb6177824dd
Gluttony <17689458313@163.com>
2023-10-14 23:15:12 +0800

    提交

---
+++ foundation/day1/1.py
@@ -0,0 +1,19 @@
+print ("hello")
+print ("----------------")
+# 单行注释
+'''
+多行注释
+变量:由数字 字母 下划线组成，不要使用下划线开头
+不能和关键字重名
+'''
+
+number = 20 + 20
+number_1 = 20 - 10
+number_2 = 20 * 10
+number_3 = 10 / 20
+number_4 = 9 // 4
+number_5 = 9 % 2
+number_6 = 2 ** 3
+print ("加法 %s \n减法 %s \n乘法 %s \n除法 %s \n取整 %s \n取余 %s \n幂 %s"  %  \
+       (number,number_1,number_2,number_3,number_4,number_5,number_6))
+
